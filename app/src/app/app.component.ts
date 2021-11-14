4d075f0dc07a2807a37af53c71ae3c256413934c
Thomas <hi@thomasmichaeler.com>
2021-11-14 22:11:28 +0100

    more sutff and more code

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -14,21 +14,21 @@ export class AppComponent implements OnInit {
   faSearch = faSearch;
   faCheck = faCheck;
 
-  public todos = [
-    { desc: "First todo to do", status: "open" },
-    { desc: "This would also be great", status: "done" },
-    { desc: "Can you please buy milk?", status: "open" },
-    { desc: "And bread?", status: "open" },
-    { desc: "What about eggs?", status: "open" },
-    { desc: "Oh I like ham", status: "open" }
+  public notes = [
+    { title: "Title of the note", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", category: "blue" },
+    { title: "Title of the note", desc: "This would also be great. \n But I'd still like to figure it out.", category: "blue" },
+    { title: "Title of the note", desc: "Can you please buy milk?", category: "red" },
+    { title: "Title of the note", desc: "And bread?", category: "blue" }
   ];
 
-  public isDone = false;
-
-  public checkDone() {
-     // reverse the value of property
-     this.isDone = !this.isDone;
-  }
+  public todos = [
+    { desc: "First todo to do", category: "blue", status: "open" },
+    { desc: "This would also be great", category: "blue", status: "done" },
+    { desc: "Can you please buy milk?", category: "red", status: "open" },
+    { desc: "And bread?", category: "blue", status: "open" },
+    { desc: "What about eggs?", category: "yellow", status: "open" },
+    { desc: "Oh I like ham", category: "blue", status: "open" }
+  ];
   
   constructor(){}
 

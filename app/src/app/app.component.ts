11004452a5578d507faa39d9d81c97e30d0a3917
Thomas <hi@thomasmichaeler.com>
2021-11-14 21:26:09 +0100

    now with dynamic todos

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faAlignRight, faSearch, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -6,9 +6,34 @@ import { faAlignRight, faSearch, faCheck } from '@fortawesome/free-solid-svg-ico
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
   title = 'app';
   faAlignRight = faAlignRight;
   faSearch = faSearch;
   faCheck = faCheck;
+
+  public todos = [
+    { desc: "First todo to do", status: "open" },
+    { desc: "This would also be great", status: "done" },
+    { desc: "Can you please buy milk?", status: "open" },
+    { desc: "And bread?", status: "open" },
+    { desc: "What about eggs?", status: "open" },
+    { desc: "Oh I like ham", status: "open" }
+  ];
+
+  public isDone = false;
+
+  public checkDone() {
+     // reverse the value of property
+     this.isDone = !this.isDone;
+  }
+  
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
+
 }
